e927fdc5f05b7665ce8f29c08d567dc25bfc8a78
hussamh10 <hussamh10@gmail.com>
2018-04-03 11:33:04 +0500

    Added resume

--- resume.py
+++ resume.py
@@ -2,7 +2,6 @@ from time import time
 from enet import myUnet
 
 enet = myUnet(224, 224)
-enet.load_weights(' osama put hdf5 name here')
 
 enet.train(170, 'osama data directory here', '', '.jpg', str(time()))
 
